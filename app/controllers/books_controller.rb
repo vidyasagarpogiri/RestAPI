8625d7a66e6e27189d816832a38bcad452490fff
unknown <vidya.pogiri@kinnser.com>
2016-07-15 19:32:31 +0530

    added edit action in books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+before_action :set_book, only: [:show, :edit, :update, :destroy]
   def show
   end
 
@@ -12,12 +13,12 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(student_params)
+    @book = Book.new(book_params)
 
     respond_to do |format|
       if @book.save
         format.html { redirect_to @book, notice: 'Book was successfully created.' }
-        format.json { render :show, status: :created, location: @book }
+        format.json { render json: @book.to_json, status: :created, location: @book }
       else
         format.html { render :new }
         format.json { render json: @book.errors, status: :unprocessable_entity }
@@ -29,18 +30,38 @@ class BooksController < ApplicationController
   end
 
   def update
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        format.json { render json: @book.to_json, status: :ok, location: @student }
+      else
+        format.html { render :edit }
+        format.json { render json: @book.to_json, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+	#raise @book.inspect
+	if @book.present?
+    @book.destroy
+    respond_to do |format|
+      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
+      format.json { render :json => @book.to_json, :status => :unprocessable_entity } 
+    end
+	else 
+ msg = { :status => "ok", :message => "Success!", :html => "<b>...</b>" }
+    format.json  { render :json => msg } # don't do msg.to_json
+     end
   end
   
     private
 
-    def set_student
+    def set_book
       @book = Book.find(params[:id])
     end
 
     def book_params
-      params.require(:book).permit(:Title, :Author, :Price)
+      params.require(:book).permit(:title, :author, :price)
     end
 end
