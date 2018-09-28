198a6fc25a384b931c18eb3e9798eef77296c190
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-09-28 13:55:09 +0530

    before_action
    before_action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
 before_action :set_book, only: [:show, :edit, :update, :destroy]
     
-  # show the book details                
+  # show the book details                   
   def show             
   end
                                                        
